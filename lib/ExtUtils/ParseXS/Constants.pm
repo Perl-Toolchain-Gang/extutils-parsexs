480a0bcc9a63574c4b87f0275fca1df104743d89
Steffen Mueller <smueller@cpan.org>
2015-08-10 10:37:37 +0200

    Partial version bump for change grafting

--- lib/ExtUtils/ParseXS/Constants.pm
+++ lib/ExtUtils/ParseXS/Constants.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use Symbol;
 
-our $VERSION = '3.25';
+our $VERSION = '3.27';
 
 =head1 NAME
 
--- lib/ExtUtils/ParseXS/CountLines.pm
+++ lib/ExtUtils/ParseXS/CountLines.pm
@@ -1,7 +1,7 @@
 package ExtUtils::ParseXS::CountLines;
 use strict;
 
-our $VERSION = '3.25';
+our $VERSION = '3.27';
 
 our $SECTION_END_MARKER;
 
